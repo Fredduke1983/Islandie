fa1486b9b3c8ab1a8119f1c074eda9ba13a9ba6b
Ihor Nechai <fredduke1983@gmail.com>
2025-04-02 23:20:26 +0300

    add first classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.location.GameField;
+import org.example.entities.Organism;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        GameField gameField = GameField.getInstance();
+        gameField.init();
+
+        System.out.println(gameField.getCell(2, 4).getOrganisms());
+        List<ArrayList<Organism>> organisms = gameField.getCell(2, 4).getOrganisms();
+        System.out.println(organisms.get(0).size());
+        System.out.println(organisms.get(0).remove(0));
+        System.out.println(organisms.get(0).size());
+
+
     }
 }
